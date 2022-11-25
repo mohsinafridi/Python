a25ed5bbc3d7f0cedbe6d19e9e72adf24ab40741
Mohsin Azam <mohsin.azam@maqta.ae>
2022-11-25 15:57:35 +0400

    first commit

---
+++ CH-01/types.py
@@ -0,0 +1,20 @@
+int_a=7
+bool_b=True
+
+int_a = 8
+print(int_a + 5)
+print(bool_b)
+
+
+integer = 7
+float_num = 2.718 
+bool_val = False
+integer += 10
+
+
+add=100
+add+=10
+
+print(15 / 3 * 2 * 2 - (3 + 4))
+
+# This is just Comment.
---
+++ CH-01/variable.py
@@ -0,0 +1,4 @@
+ex_var=5
+print("value is ", ex_var)
+
+exCamm = 5
